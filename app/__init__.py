ad5a51f57c453352b62a18c60ba1e3728f2a74ab
khalidhmd <khalidhmd@outlook.com>
2018-03-20 22:56:10 +0200

    Postgre5

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,10 @@ db = SQLAlchemy(app)
 
 from app import stores, dummy_data
 
-member_store = stores.MemberStore()
-post_store = stores.PostStore()
+member_store = dummy_data.dummy_members
+post_store = dummy_data.dummy_posts
 
-dummy_data.seed_stores()
+dummy_data.seed_stores(member_store, post_store)
 
 from app import views
 from app import api
